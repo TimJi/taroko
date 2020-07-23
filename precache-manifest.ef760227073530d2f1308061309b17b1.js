2cc4767ec33a0096d4bc680c8bb7bda97efd0096
Tim <timji0210@gmail.com>
2020-07-23 23:54:29 +0800

    Updates

---
+++ precache-manifest.ef760227073530d2f1308061309b17b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62c537f3cd980c9811d9b98d09dafacb",
+    "url": "/taroko/index.html"
+  },
+  {
+    "revision": "7a964f759823608cb8e9",
+    "url": "/taroko/static/css/main.87075b1a.chunk.css"
+  },
+  {
+    "revision": "6c72546baa86f8e9e210",
+    "url": "/taroko/static/js/2.27e918e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/taroko/static/js/2.27e918e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a964f759823608cb8e9",
+    "url": "/taroko/static/js/main.69d61670.chunk.js"
+  },
+  {
+    "revision": "3f36570d7ca133464325",
+    "url": "/taroko/static/js/runtime-main.45aa3bb7.js"
+  }
+]);

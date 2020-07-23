5b2c4268b4de28778646ae0fa5b1b49edfc916c5
Tim <timji0210@gmail.com>
2020-07-23 23:43:32 +0800

    basic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
+  const arr = ['請至文天祥公園接受任務', '快去文天祥公園接受任務', '再不去接受任務就來不及了', '你還在看？']
+  let count = 0;
+  setInterval( function() {
+    count += 1;
+    const typewriteAni = document.getElementById('typewrite-animated');
+    const typewriteText = document.getElementById('typewrite-text');
+    if (typewriteAni.classList.contains('typewriter')) {
+      typewriteAni.classList.remove('typewriter');
+    } else {
+      typewriteAni.classList.add('typewriter');
+      typewriteAni.addEventListener('animationend', function() {
+        setTimeout(function() {
+          typewriteText.textContent = '';
+          typewriteAni.classList.remove('typewriter');
+        }, 800)
+      })
+    }
+    typewriteText.textContent = arr[count%3];
+  }, 2500);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="d-inline-block app">
+        <div id="typewrite-animated">
+          <span id="typewrite-text" className="d-inline-block"/>
+        </div>
+      </div>
   );
 }
 
